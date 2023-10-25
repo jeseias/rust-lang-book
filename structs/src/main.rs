aebddf914841ec2a4c89ca5f93d4e26851ab6b68
jeseias <domingosjeseias@gmail.com>
2023-10-25 08:45:43 +0100

    feat: structs

---
+++ structs/src/main.rs
@@ -0,0 +1,63 @@
+struct User {
+  username: String,
+  email: String,
+  sign_in_count: u64,
+  active: bool,
+}
+
+#[derive(Debug)]
+struct Rectangle {
+  width: u32,
+  height: u32
+}
+
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
+  }
+
+  fn can_hold(&self, other: &Rectangle) -> bool { 
+    self.width > other.width && self.height > other.height
+  }
+}
+
+impl Rectangle {
+  fn square (size: u32) -> Rectangle {
+    Rectangle { width: size, height: size }
+  }
+}
+
+fn main() {
+  let rect = Rectangle {
+    width: 30,
+    height: 50
+  };
+
+  let rect1 = Rectangle {
+    width: 20, 
+    height: 40
+  };
+
+  let rect2 = Rectangle {
+    width: 40, 
+    height: 50 
+  };
+
+  let rect3 = Rectangle::square(25);
+
+
+  println!("rect can hold rect1: {}", rect.can_hold(&rect1));
+  println!("rect can hold rect1: {}", rect.can_hold(&rect2));
+
+  println!("rect: {:#?}", rect);
+  println!("The area of the reactngle is {} square pixels.", rect.area() );
+}
+
+fn build_user(email: String, username: String) -> User {
+  User {
+    email,
+    username,
+    active: true,
+    sign_in_count: 1
+  }
+}
